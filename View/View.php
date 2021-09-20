a8115793803c416021e477ed4d7cffa090a9aa98
Diego Arreceygor <darreceygor@gmail.com>
2021-09-20 16:40:09 -0300

    Correcion en visualizacion: listado con Bootstrap

--- View/View.php
+++ View/View.php
@@ -15,28 +15,47 @@ class View {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
             <title>Listado</title>
         </head>
         <body>
             
             <h1>Listado de Doctores</h1>
         
-            <ul>';
-            
-            echo '<pre>';
-            var_dump($doctors); 
-            echo '</pre>';
-            echo'<br>';
-            
-            foreach($doctors as $doctor) {
-                '<li>'. $doctor->name .': '.$doctor->surname .' - '.'</li>';
-            }
-    
-        '
-            </ul>
+                <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Consultorio</th>
+                    <th scope="col">Direccion</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>';
+
+                foreach($doctors as $doctor) {
+                    echo '<th scope="row">'.$doctor->ID_doctors.'</th>
+                    <td>'. $doctor->name .'</td>
+                    <td>'.$doctor->surname .'</td>
+                    <td>'.$doctor->dr_office .'</td>
+                    <td>'.$doctor->direction .'</td>
+                </tr>';
+                    
+                }
+                
+'
+                </tbody>
+            </table>
+
+
+
+
+
+ 
          
-              
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
         </body>
         </html>';
     
